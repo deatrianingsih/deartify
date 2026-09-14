77f00109779ce0d3d1975e301d1b2e9ba7a7f998
dea <deatriya1@gmail.com>
2026-09-14 20:41:19 +0700

    Tambah gambar pada harga jasa dan memperbaiki tampilan harga jasa

--- database/seeders/ServicePriceSeeder.php
+++ database/seeders/ServicePriceSeeder.php
@@ -14,11 +14,8 @@ class ServicePriceSeeder extends Seeder
             ['name' => 'Line Art', 'description' => 'Gambar line art rapi tanpa warna, satu karakter.', 'price' => 70000],
             ['name' => 'Full Color', 'description' => 'Gambar full color lengkap dengan shading, satu karakter.', 'price' => 150000],
             ['name' => 'Chibi', 'description' => 'Gambar karakter gaya chibi, imut dan sederhana.', 'price' => 80000],
-            ['name' => 'Background', 'description' => 'Gambar latar belakang/pemandangan tanpa karakter.', 'price' => 30000],
             ['name' => 'Semi Realis', 'description' => 'Gambar karakter gaya semi realis, satu karakter.', 'price' => 150000],
             ['name' => 'Portrait Realis', 'description' => 'Gambar wajah/portrait realis dari foto referensi.', 'price' => 250000],
-            ['name' => 'Icon/Avatar', 'description' => 'Gambar ikon profil bulat, cocok untuk media sosial.', 'price' => 40000],
-            ['name' => 'Emote/Sticker', 'description' => 'Gambar emote/sticker ekspresif untuk chat atau streaming.', 'price' => 35000],
             ['name' => 'Couple/Group Art', 'description' => 'Gambar dua karakter atau lebih dalam satu ilustrasi.', 'price' => 200000],
         ];
 
